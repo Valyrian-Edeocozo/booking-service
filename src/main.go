0fd816a62e1443fcaed448cb37e222365eb21423
medallion <utobonnyb@gmail.com>
2024-09-16 22:11:30 +0100

    conditional logic flow

--- src/main.go
+++ src/main.go
@@ -33,13 +33,17 @@ func main() {
 	
 		fmt.Println("Enter the number of tickets you want to buy")
 		fmt.Scan(&userTickets)
+
+		if userTickets > remainingTickets {
+            fmt.Printf("Sorry, we only have %v tickets left\n", remainingTickets)
+            continue
+        }
 	
 		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	
-		remainingTickets = conferenceTickets - userTickets
+		remainingTickets = remainingTickets - userTickets
 	
-		fmt.Printf("The remaining ticket is %v", remainingTickets)
-
+		fmt.Printf("The remaining ticket is %v\n", remainingTickets)
 
 		bookings = append(bookings, firstName + " " + lastName)
 
@@ -50,6 +54,12 @@ func main() {
 			firstNames = append(firstNames, names[0])
 		}
 
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			fmt.Println("Sorry, all tickets are sold out. Come back next year")
+            break
+		}
 
 	}
 }
